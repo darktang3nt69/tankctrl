1782c4d6ba8a634c39a813e2884b36b222db0029
lokesh <lokeshreddy19997@gmail.com>
2026-08-31 08:58:32 +0530

    feat: map tokens to shadcn CSS variables, add light mode via next-themes

--- tankctl-web/src/App.tsx
+++ tankctl-web/src/App.tsx
@@ -1,7 +1,7 @@
+import { ThemeProvider } from 'next-themes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LiveEventsProvider } from './ws/LiveEventsProvider'
-import { ToastProvider } from './components/Toast'
 import { AppShell } from './components/AppShell'
 import { Overview } from './routes/Overview'
 import { TankDetail } from './routes/TankDetail'
@@ -9,6 +9,8 @@ import { Alerts } from './routes/Alerts'
 import { Settings } from './routes/Settings'
 import { NotFound } from './routes/NotFound'
 import { useGlobalLiveSync } from './ws/useGlobalLiveSync'
+import { Toaster } from './components/ui/sonner'
+import { TooltipProvider } from './components/ui/tooltip'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,23 +28,26 @@ function GlobalLiveSync() {
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <LiveEventsProvider>
-          <ToastProvider>
-            <GlobalLiveSync />
-            <Routes>
-              <Route element={<AppShell />}>
-                <Route index element={<Overview />} />
-                <Route path="tanks/:deviceId" element={<TankDetail />} />
-                <Route path="alerts" element={<Alerts />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
-          </ToastProvider>
-        </LiveEventsProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <LiveEventsProvider>
+            <TooltipProvider>
+              <GlobalLiveSync />
+              <Toaster />
+              <Routes>
+                <Route element={<AppShell />}>
+                  <Route index element={<Overview />} />
+                  <Route path="tanks/:deviceId" element={<TankDetail />} />
+                  <Route path="alerts" element={<Alerts />} />
+                  <Route path="settings" element={<Settings />} />
+                  <Route path="*" element={<NotFound />} />
+                </Route>
+              </Routes>
+            </TooltipProvider>
+          </LiveEventsProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
